91189fd92d511a7f7a1e9f0b994893b1cd00e2e0
Dougal Matthews <dougal@dougalmatthews.com>
2018-09-04 13:14:17 +0100

    Display the local and remote hash on a conflict
    Generally improve the logging output also.

--- dropbox-upload/upload.py
+++ dropbox-upload/upload.py
@@ -69,8 +69,14 @@ def upload_file(dbx, file_path, dest_path):
         session_id=upload_session_start_result.session_id, offset=f.tell()
     )
     commit = dropbox.files.CommitInfo(path=dest_path)
+    prev = None
     while f.tell() < file_size:
-        LOG.info(f"{round((f.tell() / file_size)*100,1)}%")
+        percentage = round((f.tell() / file_size)*100)
+
+        if not prev or percentage > prev + 5:
+            LOG.info(f"{percentage:3} %")
+            prev = percentage
+
         if (file_size - f.tell()) <= CHUNK_SIZE:
             dbx.files_upload_session_finish(f.read(CHUNK_SIZE), cursor, commit)
         else:
@@ -78,6 +84,7 @@ def upload_file(dbx, file_path, dest_path):
                 f.read(CHUNK_SIZE), cursor.session_id, cursor.offset
             )
             cursor.offset = f.tell()
+    LOG.info("100 %")
 
 
 def compute_dropbox_hash(filename):
@@ -97,15 +104,22 @@ def file_exists(dbx, file_path, dest_path):
         metadata = dbx.files_get_metadata(dest_path)
     except Exception:
         LOG.info("No existing snapshot in dropox with this name")
-        return
+        return False
 
-    if compute_dropbox_hash(file_path) == metadata.content_hash:
+    dropbox_hash = metadata.content_hash
+    local_hash = compute_dropbox_hash(file_path)
+    if local_hash == dropbox_hash:
         return True
 
     # If the hash doesn't match, delete the file so we can re-upload it.
+    # We might want to make this optional? a safer mode might be to 
+    # add a suffix?
     LOG.warn(
         "The snapshot conflicts with a file name in dropbox, the contents "
-        "are different. The dropbox file will be deleted and replaced."
+        "are different. The dropbox file will be deleted and replaced. "
+        "Local hash: %s, Dropbox hash: %s",
+        local_hash,
+        dropbox_hash,
     )
     dbx.files_delete(dest_path)
     return False
@@ -136,7 +150,6 @@ def main():
                 LOG.info("Already found the upload with the same contents")
                 continue
             upload_file(dbx, snapshot, target)
-            LOG.info("Done")
         except Exception:
             LOG.exception("Upload failed")
 
