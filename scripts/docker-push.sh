3c228234e559d971163c93defe75601882f006d8
Dougal Matthews <dougalmatthews@dougals-mbp.home>
2018-10-05 20:06:50 +0100

    Add a script to docker push

---
+++ scripts/docker-push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -evx
+set -x
+
+repo=$1;
+
+docker run \
+  --privileged \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$repo":/docker \
+  hassioaddons/build-env:latest \
+  --image "hassioaddons/dropbox-upload-{arch}" \
+  --target dropbox-upload \
+  --tag-latest \
+  --push \
+  --all
