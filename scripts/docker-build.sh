5fe5dbc333f02e77230f2a544189cb9d8fa2ece9
Dougal Matthews <dougalmatthews@dougals-mbp.home>
2018-10-05 20:22:34 +0100

    Update the docker hub org

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -10,7 +10,7 @@ docker run \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$repo":/docker \
   hassioaddons/build-env:latest \
-  --image "hassioaddons/dropbox-upload-{arch}" \
+  --image "d0ugal/dropbox-upload-{arch}" \
   --cache-tag test \
   --git \
   --target dropbox-upload \
