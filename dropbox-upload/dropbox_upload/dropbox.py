f5a012926931bb9e46c84520b30df8b6442a969c
Dougal Matthews <dougal@dougalmatthews.com>
2020-08-14 19:42:19 +0100

    Fix a minor bug with percentage printing

--- dropbox-upload/dropbox_upload/dropbox.py
+++ dropbox-upload/dropbox_upload/dropbox.py
@@ -35,7 +35,7 @@ def upload_file(dbx, file_path, dest_path):
     while f.tell() < file_size:
         percentage = round((f.tell() / file_size) * 100)
 
-        if not prev or percentage > prev + 5:
+        if prev is None or percentage > prev + 5:
             LOG.info(f"{percentage:3} %")
             prev = percentage
 
